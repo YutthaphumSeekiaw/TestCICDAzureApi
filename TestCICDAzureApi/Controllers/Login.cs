d46f365fc52c792c8aa1083207aff1d3b289a151
yutthsee <yutthsee@scg.com>
2022-07-23 15:08:04 +0700

    update

--- TestCICDAzureApi/Controllers/Login.cs
+++ TestCICDAzureApi/Controllers/Login.cs
@@ -13,7 +13,7 @@ namespace TestCICDAzureApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","AAA"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
